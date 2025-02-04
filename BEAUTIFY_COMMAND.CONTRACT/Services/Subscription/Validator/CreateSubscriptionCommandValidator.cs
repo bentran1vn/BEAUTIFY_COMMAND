4384fef8aa2908a9c5bf6e30a80942fdb0629daf
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-02-04 17:48:51 +0700

    Merge pull request #1
    * Add validators for subscription commands and refactor SubscriptionPac…

---
+++ BEAUTIFY_COMMAND.CONTRACT/Services/Subscription/Validator/CreateSubscriptionCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+
+namespace BEAUTIFY_COMMAND.CONTRACT.Services.Subscription.Validator;
+internal sealed class CreateSubscriptionCommandValidator : AbstractValidator<Commands.CreateSubscriptionCommand>
+{
+    public CreateSubscriptionCommandValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MinimumLength(2).WithMessage("Name must be at least 2 characters long")
+            .MaximumLength(50).WithMessage("Name must exceed 50 characters")
+            .Matches("^[a-zA-Z0-9 ]*$").WithMessage("Name must contain only letters and numbers");
+
+        RuleFor(x => x.Description)
+            .NotEmpty()
+            .MinimumLength(10).WithMessage("Description must be at least 10 characters long")
+            .MaximumLength(200).WithMessage("Description must exceed 200 characters");
+
+        RuleFor(x => x.Price)
+            .NotEmpty()
+            .GreaterThan(1000).WithMessage("Price must be greater than 1000");
+            
+
+        RuleFor(x => x.Duration)
+            .NotEmpty()
+            .GreaterThan(0).WithMessage("Duration must be greater than 0");
+    }
+}
