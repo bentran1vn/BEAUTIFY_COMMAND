071033970646a3585962a35ae8192480086d1d66
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-02-10 01:09:16 +0700

    Disable antiforgery for CreateDoctorCertificate endpoint and update command binding

--- BEAUTIFY_COMMAND.PRESENTATION/APIs/DoctorCertificate/DoctorCertificateEndpoint.cs
+++ BEAUTIFY_COMMAND.PRESENTATION/APIs/DoctorCertificate/DoctorCertificateEndpoint.cs
@@ -8,14 +8,14 @@ public class DoctorCertificateEndpoint : ApiEndpoint, ICarterModule
     public void AddRoutes(IEndpointRouteBuilder app)
     {
         var gr1 = app.NewVersionedApi("Doctor Certificates").MapGroup(BaseUrl).HasApiVersion(1);
-        gr1.MapPost(string.Empty, CreateDoctorCertificate);
+        gr1.MapPost(string.Empty, CreateDoctorCertificate).DisableAntiforgery();
         gr1.MapPut("{id:guid}", UpdateDoctorCertificate);
         gr1.MapDelete("{id:guid}", DeleteDoctorCertificate);
     }
 
     private static async Task<IResult> CreateDoctorCertificate(
         ISender sender,
-        Commands.CreateDoctorCertificateCommand command)
+        [FromForm] Commands.CreateDoctorCertificateCommand command)
     {
         var result = await sender.Send(command);
         return result.IsFailure ? HandlerFailure(result) : Results.Ok(result);
