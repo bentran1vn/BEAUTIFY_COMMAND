f4587a7d473c9709a4c5e35908345ce0b470cc7e
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-03-16 13:59:08 +0700

    Refactor ClinicApi endpoints for consistency; update request/response models and improve naming conventions

--- BEAUTIFY_COMMAND.PRESENTATION/APIs/Clinics/ClinicApi.cs
+++ BEAUTIFY_COMMAND.PRESENTATION/APIs/Clinics/ClinicApi.cs
@@ -13,110 +13,86 @@ public class ClinicApi : ApiEndpoint, ICarterModule
         var gr1 = app.NewVersionedApi("Clinics")
             .MapGroup(BaseUrl).HasApiVersion(1);
 
-        gr1.MapPost("application", ClinicApply)
+        gr1.MapPost("", ClinicApply)
             .DisableAntiforgery()
-            .WithName("apply")
-            .WithSummary("New Clinic send a request to Join System.")
-            .WithDescription("Clinics can send a request to join a clinic." +
-                             " Clinic can apply multiple times when clinic request was rejected (Each apply time is" +
-                             " 30 days apart). If Clinic is banned user can not send request again." +
-                             "A clinics when apply again must send email, phone number, tax code same with the last request.")
+            .WithName("ApplyClinic")
+            .WithSummary("Create a new clinic request")
             .WithOpenApi(operation => new OpenApiOperation(operation)
+            {
+                RequestBody = new OpenApiRequestBody
                 {
-                    RequestBody = new OpenApiRequestBody
+                    Content =
                     {
-                        Content =
+                        ["multipart/form-data"] = new OpenApiMediaType
                         {
-                            ["multipart/form-data"] = new OpenApiMediaType
+                            Schema = new OpenApiSchema
                             {
-                                Schema = new OpenApiSchema
+                                Type = "object",
+                                Properties = new Dictionary<string, OpenApiSchema>
                                 {
-                                    Type = "object",
-                                    Properties = new Dictionary<string, OpenApiSchema>
+                                    ["name"] = new()
+                                        { Type = "string", Example = new OpenApiString("Thẩm mĩ viện Hướng Dương") },
+                                    ["email"] = new()
                                     {
-                                        {
-                                            "Name",
-                                            new OpenApiSchema
-                                            {
-                                                Type = "string", Example = new OpenApiString("Thẩm mĩ viện Hướng Dương")
-                                            }
-                                        },
-                                        {
-                                            "Email",
-                                            new OpenApiSchema
-                                            {
-                                                Type = "string", Format = "email",
-                                                Example = new OpenApiString("tan11105@gmail.com")
-                                            }
-                                        },
-                                        {
-                                            "PhoneNumber",
-                                            new OpenApiSchema
-                                            {
-                                                Type = "string", Format = "phone",
-                                                Example = new OpenApiString("+84983460123")
-                                            }
-                                        },
-                                        {
-                                            "Address",
-                                            new OpenApiSchema
-                                                { Type = "string", Example = new OpenApiString("Biên Hoà, Đồng Nai") }
-                                        },
-                                        {
-                                            "TaxCode",
-                                            new OpenApiSchema { Type = "string", Example = new OpenApiString("123123") }
-                                        },
-                                        { "BusinessLicense", new OpenApiSchema { Type = "string", Format = "binary" } },
-                                        {
-                                            "OperatingLicense", new OpenApiSchema { Type = "string", Format = "binary" }
-                                        },
-                                        {
-                                            "OperatingLicenseExpiryDate",
-                                            new OpenApiSchema
-                                            {
-                                                Type = "string", Format = "date",
-                                                Example = new OpenApiString("2025-12-31")
-                                            }
-                                        },
-                                        {
-                                            "ProfilePictureUrl",
-                                            new OpenApiSchema { Type = "string", Format = "binary" }
-                                        }
-                                    }
+                                        Type = "string", Format = "email",
+                                        Example = new OpenApiString("tan11105@gmail.com")
+                                    },
+                                    ["phone_number"] = new()
+                                    {
+                                        Type = "string", Format = "phone", Example = new OpenApiString("+84983460123")
+                                    },
+                                    ["address"] = new()
+                                        { Type = "string", Example = new OpenApiString("Biên Hoà, Đồng Nai") },
+                                    ["tax_code"] = new() { Type = "string", Example = new OpenApiString("123123") },
+                                    ["business_license"] = new() { Type = "string", Format = "binary" },
+                                    ["operating_license"] = new() { Type = "string", Format = "binary" },
+                                    ["operating_license_expiry_date"] = new()
+                                        { Type = "string", Format = "date", Example = new OpenApiString("2025-12-31") },
+                                    ["profile_picture_url"] = new() { Type = "string", Format = "binary" }
                                 }
                             }
                         }
                     }
                 }
-            );
+            });
 
-        gr1.MapPut("application/{id}", ResponseClinicApply)
-            .WithName("Response Apply Request")
-            .WithSummary("Admin Response Apply Request.")
-            .WithDescription("With Action = 0 is Approve, Action = 1 is Reject, Action = 2 is Banned." +
-                             " With Action 1 and 2, reject reason must be included." +
-                             " Id in the path with RequestId in the request body must be same.");
-        // .RequireAuthorization()
+        gr1.MapPut("{id}/response", ResponseClinicApply)
+            .WithName("RespondToApplyRequest")
+            .WithSummary("Admin response to clinic request")
+            .WithDescription(
+                "Action = 0 (Approve), Action = 1 (Reject), Action = 2 (Ban). Reject reason is required for Action 1 and 2.")
+            .RequireAuthorization();
 
         gr1.MapPut("{id}", UpdateClinic)
             .DisableAntiforgery()
-            .WithName("Update Clinic Information")
-            .WithSummary("Update Clinic Information.")
-            .WithDescription("")
+            .WithName("UpdateClinic")
+            .WithSummary("Update clinic information")
             .RequireAuthorization();
 
-        gr1.MapPost("clinic-account", ClinicCreateAccountForEmployee);
-        gr1.MapDelete("clinic-account", ClinicRemoveAccountForEmployee);
-        gr1.MapPut("clinic-account", ClinicUpdateAccountOfEmployeeCommand).DisableAntiforgery();
-        gr1.MapPut("staff-change-doctor-working-clinic", StaffChangeDoctorWorkingClinic).DisableAntiforgery();
-        gr1.MapPost("create-branch", ClinicCreateBranch).DisableAntiforgery().RequireAuthorization();
-        gr1.MapPut("update-branch", ClinicUpdateBranch).DisableAntiforgery().RequireAuthorization();
-        gr1.MapDelete("delete-branch", ClinicDeleteBranch).DisableAntiforgery().RequireAuthorization();
-        gr1.MapPatch("{ClinicId:guid}/status", ClinicUpdateStatus)
+        gr1.MapPost("{id}/accounts", ClinicCreateAccountForEmployee)
+            .RequireAuthorization();
+        gr1.MapDelete("{id}/accounts", ClinicRemoveAccountForEmployee)
+            .RequireAuthorization();
+        gr1.MapPut("{id}/accounts", ClinicUpdateAccountOfEmployeeCommand)
+            .DisableAntiforgery()
+            .RequireAuthorization();
+        gr1.MapPatch("{id}/status", ClinicUpdateStatus)
+            .DisableAntiforgery()
+            .RequireAuthorization();
+        gr1.MapPatch("{id}/staff/doctor", StaffChangeDoctorWorkingClinic)
+            .DisableAntiforgery()
+            .RequireAuthorization();
+        gr1.MapPost("{id}/branches", ClinicCreateBranch)
+            .DisableAntiforgery()
+            .RequireAuthorization();
+
+        gr1.MapPut("{id}/branches/{branchId}", ClinicUpdateBranch)
+            .DisableAntiforgery()
+            .RequireAuthorization();
+        gr1.MapDelete("{id}/branches/{branchId}", ClinicDeleteBranch)
             .DisableAntiforgery()
             .RequireAuthorization();
     }
-
 
     private static async Task<IResult> StaffChangeDoctorWorkingClinic(ISender sender,
         [FromForm] Commands.StaffChangeDoctorWorkingClinicCommand command)
@@ -204,9 +180,9 @@ public class ClinicApi : ApiEndpoint, ICarterModule
         return result.IsFailure ? HandlerFailure(result) : Results.Ok(result);
     }
 
-    private static async Task<IResult> ClinicUpdateStatus(ISender sender, Guid ClinicId)
+    private static async Task<IResult> ClinicUpdateStatus(ISender sender, Guid clinicId)
     {
-        var result = await sender.Send(new Commands.ChangeClinicActivateStatusCommand(ClinicId));
+        var result = await sender.Send(new Commands.ChangeClinicActivateStatusCommand(clinicId));
 
         return result.IsFailure ? HandlerFailure(result) : Results.Ok(result);
     }
