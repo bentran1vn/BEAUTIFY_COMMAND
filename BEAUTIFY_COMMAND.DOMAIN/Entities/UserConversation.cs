02befec35c8a5c0191a0f08c667a0df96bceaf04
Eric <36512385+PhucNghi176@users.noreply.github.com>
2025-04-10 18:17:18 +0700

    refactor: Make UserConversation and Message entities support staff members
    The changes modify the `UserConversation` and `Message` entities to better support staff members in the conversation system.
    
    In `UserConversation`, the `UserId` is made nullable and a new `StaffId` property with corresponding `Staff` navigation property is added.
    
    In `Message`, the `Sender` property is commented out and replaced with an `IsStaff` flag to distinguish between user and staff senders. These changes enable the system to handle conversations involving both users and staff members.

--- BEAUTIFY_COMMAND.DOMAIN/Entities/UserConversation.cs
+++ BEAUTIFY_COMMAND.DOMAIN/Entities/UserConversation.cs
@@ -1,8 +1,11 @@
 ﻿namespace BEAUTIFY_COMMAND.DOMAIN.Entities;
 public class UserConversation : AggregateRoot<Guid>, IAuditableEntity
 {
-    public Guid UserId { get; set; }
-    public virtual User User { get; set; } = null!;
+    public Guid? UserId { get; set; }
+    public virtual User? User { get; set; } = null!;
+
+    public Guid? StaffId { get; set; }
+    public virtual Staff? Staff { get; set; } = null!;
 
     public Guid ConversationId { get; set; }
     public virtual Conversation Conversation { get; set; } = null!;
