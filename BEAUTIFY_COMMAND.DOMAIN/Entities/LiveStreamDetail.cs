3c02fe17ebb6021ca7a6844252e95a3a63151e04
bentran1vn <tan182205@gmail.com>
2025-04-16 17:55:28 +0700

    Fix: Add Missing Field Entity

--- BEAUTIFY_COMMAND.DOMAIN/Entities/LiveStreamDetail.cs
+++ BEAUTIFY_COMMAND.DOMAIN/Entities/LiveStreamDetail.cs
@@ -7,6 +7,7 @@ public class LiveStreamDetail : AggregateRoot<Guid>, IAuditableEntity
     public int ReactionCount { get; set; }
     public int TotalActivities { get; set; }
     public int TotalBooking { get; set; }
+    public Guid LivestreamRoomId { get; set; }
     public DateTimeOffset CreatedOnUtc { get; set; }
     public DateTimeOffset? ModifiedOnUtc { get; set; }
 }
